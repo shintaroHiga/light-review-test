361a396232897d70b08238cbe11fc20686892497
shintaroHiga <148220347+shintaroHiga@users.noreply.github.com>
2023-11-17 16:12:30 +0900

    Add files via upload

---
+++ q7_kadai/calc.js
@@ -0,0 +1,16 @@
+// numの値は整数であれば何でも良いです。いろんな数字で実行結果の変化を確認しましょう。
+
+let num = 15;
+
+if ((num % 3 = 0) && (num % 5 == 0)) {
+    console.log("3と5の倍数です");
+}
+else if (num % 3 = 0) {
+    console.log("3の倍数です");
+}
+else if (num % 5 = 0) {
+    console.log("5の倍数です");
+}
+else {
+    console.log(num);
+}
